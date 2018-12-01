ad897e343e08f654585e99b8824e5154643cdc0a
UrvaApps <dexterpune@gmail.com>
2018-12-01 14:09:01 +0530

    Initial Commit

---
+++ mylibrary/src/main/java/com/urvalabs/mylibrary/Demo.java
@@ -0,0 +1,12 @@
+package com.urvalabs.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Demo {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,"Hii",Toast.LENGTH_SHORT).show();
+
+    }
+}
